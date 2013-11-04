94ebf2968bb45ac05f06c8bdacc65ba0b17a3a7c
Melinda Mizuta <mkmizuta@gmail.com>
2013-11-04 15:03:11 -0800

    started work on flavors array

--- ice_cream_planner.rb
+++ ice_cream_planner.rb
@@ -1,9 +1,10 @@
 # Get info from a user and returns a hash
 class PartyPref
-	attr_accessor :ice_cream_hash
+	attr_accessor :ice_cream_hash, :flavors
 
 	def initialize
 		@ice_cream_hash = {}
+		@flavors = []
 	end
 
 	def get_info
@@ -12,22 +13,25 @@ class PartyPref
 		puts "What is your favorite flavor of ice cream"
 		flavor = gets.chomp
 		usr_hash = {name => flavor}
+		# @flavors.push(flavor)
 	end
+
 	# Merge the hashes into a master hash
-	def merge_usr_icecream_choice(master_hash, input_hash)
-		master_hash.merge!(input_hash)
+	def merge_usr_icecream_choice(input_hash)
+		@ice_cream_hash.merge!(input_hash)
 	end
 	# Gets the hashes of multiple users
 	def get_another_user
 		puts "Are there any more guests coming?"
 		more_guests = gets.chomp
 		until more_guests == "no"
-			merge_usr_icecream_choice(@ice_cream_hash, get_info)
+			merge_usr_icecream_choice(get_info)
 			puts "Are there any more guests coming?"
 			more_guests = gets.chomp
 		end	
-		@ice_cream_hash
+		@ice_cream_hash	
 	end
 
 end
-puts PartyPref.new.get_another_user
+#puts PartyPref.new.get_another_user
+puts PartyPref.new.get_info
